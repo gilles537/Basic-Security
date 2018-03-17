a143a0efa958d601d1b784b6a13ca665c06c97d1
11600224 <11600224@5CG62129CN>
2018-03-17 12:30:45 +0100

    mainclasse output en gebruiker static methods(io)

--- Gebruiker.java
+++ Gebruiker.java
@@ -1,5 +1,51 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Gebruiker {
 	private String password;
 	private String gebruikersnaam;
+	
+	public static void GetGebruikers(){
+		try{
+			FileReader fileReader = new FileReader("Files/Gebruikers.txt");
+			BufferedReader reader = new BufferedReader(fileReader);
+			String line= null;
+			while ((line =reader.readLine())!=null){
+				
+			}
+			reader.close();
+		}catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	
+	public static void SaveGebruikers(){
+		
+		try{
+			File file = new java.io.File("Files/Gebruikers.txt");
+			file.createNewFile();
+			FileWriter writer = new FileWriter(file);
+			
+			
+		}catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	
+	public Gebruiker(String password, String gebruikersnaam) {
+		super();
+		this.password = password;
+		this.gebruikersnaam = gebruikersnaam;
+	}
+	public String getGebruikersnaam() {
+		return gebruikersnaam;
+	}
+	
+	
+	
 }
--- mainclass.java
+++ mainclass.java
@@ -1,12 +1,30 @@
+import java.util.Scanner;
 
 public class mainclass {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
+		String gebruikersnaam;
+		String boodschap;
+		String paswoord;
+		Scanner scanner= new Scanner(System.in);
+		System.out.println("Gebruikersnaam:");
+		gebruikersnaam = scanner.nextLine();
+		System.out.println("geef paswoord op");
+		paswoord= scanner.nextLine();
+		new Gebruiker(gebruikersnaam,paswoord);
+		
+		System.out.println("geef boodschap op");
+		boodschap = scanner.nextLine();	
+
+		new AES_Encryptor();
+		KeyManager keymanager = new KeyManager();
+		AES_Encryptor.encrypt(keymanager.getAESKey(), keymanager.getAESIV(), boodschap);
+		
 		String test = "Dirk";
 		AES_Encryptor encryptor = new AES_Encryptor();
-		KeyManager keymanager = new KeyManager();
+		KeyManager keymanager1 = new KeyManager();
 		
 		//String encryptedTest =  encryptor.encrypt(keymanager.getAESKey(), keymanager.getAESIV(), "Pannekoek");
 		
